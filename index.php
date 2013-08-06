c7a836f772b8e10c6664ffd1c39bb569a0ec87a2
Matthew Struble <Matt@Mayateck.net>
2013-08-06 13:47:37 -0700

    Created some defaults for landing.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/style.core.css">
+	<title>Homepage</title>
+</head>
+<body>
+	<center><div class="page">
+		TEST
+	</div></center>
+</body>
+</html>
